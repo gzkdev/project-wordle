e216a8ceb7c1d8dc8e87b734bd4bfb168f5f5726
Ezihe Godswill <ezihegodswill01@gmail.com>
2025-03-24 04:37:58 +0100

    add banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Banner({ gameStatus, guessCount, answer }) {
+  if (gameStatus === "playing") return null;
+
+  if (gameStatus === "won") {
+    return (
+      <div className="happy banner">
+        <p>
+          <strong>Congratulations!</strong> Got it in
+          <strong>{guessCount} guesses</strong>.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="sad banner">
+      <p>
+        Sorry, the correct answer is <strong>{answer}</strong>.
+      </p>
+    </div>
+  );
+}
+
+export default Banner;
